d604b8b426d76560ba24ae52b8db819aeca93707
Tyler Albee <tyal7988@colorado.edu>
2018-04-15 17:21:42 -0600

    initial commit

---
+++ ProtoSpeechEnigine/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ProtoSpeechEnigine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.AccessibleName = "Enable Voice Control";
+            this.btnEnable.Location = new System.Drawing.Point(12, 134);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(132, 34);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AccessibleName = "log";
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(443, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "- log -";
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.AccessibleName = "Disable Voice Control";
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(168, 134);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(142, 34);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "ProtoSpeechEngine";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 200);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProtoSpeechEngine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ ProtoSpeechEnigine/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace ProtoSpeechEnigine
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "hello", "hello world", "next", "previous", "prev" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "hello":
+                    richTextBox1.Text += "\nhello";
+                    break;
+                case "hello world":
+                    richTextBox1.Text += "\nhello world";
+                    break;
+                case "next":
+                    richTextBox1.Text += "\nnext";
+                    break;
+                case "previous":
+                    richTextBox1.Text += "\nprevious";
+                    break;
+                case "prev":
+                    richTextBox1.Text += "\nprev";
+                    break;
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+    }
+}
