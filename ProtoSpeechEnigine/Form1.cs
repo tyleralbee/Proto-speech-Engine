39b62cedb76783b2a0e8e2f18c355a3f44970bf5
Tyler Albee <tyal7988@colorado.edu>
2018-04-15 18:11:13 -0600

    mode addition

--- ProtoSpeechEnigine/Form1.cs
+++ ProtoSpeechEnigine/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +14,10 @@ namespace ProtoSpeechEnigine
     public partial class Form1 : Form
     {
         SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine recEngine2 = new SpeechRecognitionEngine();
+        SpeechRecognitionEngine recEngine3 = new SpeechRecognitionEngine();
+
+
         public Form1()
         {
             InitializeComponent();
@@ -27,37 +31,84 @@ namespace ProtoSpeechEnigine
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Choices commands = new Choices();
-            commands.Add(new string[] { "hello", "hello world", "next", "previous", "prev" });
-            GrammarBuilder gBuilder = new GrammarBuilder();
-            gBuilder.Append(commands);
-            Grammar grammar = new Grammar(gBuilder);
+            
+            Choices states = new Choices();
+            states.Add(new string[] { "review mode", "edit mode" });
 
-            recEngine.LoadGrammarAsync(grammar);
+            GrammarBuilder sBuilder = new GrammarBuilder();
+            sBuilder.Append(states);
+            Grammar stateGrammar = new Grammar(sBuilder);
+            recEngine.LoadGrammarAsync(stateGrammar);
             recEngine.SetInputToDefaultAudioDevice();
             recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+            
         }
 
         private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            switch (e.Result.Text)
-            {
-                case "hello":
-                    richTextBox1.Text += "\nhello";
-                    break;
-                case "hello world":
-                    richTextBox1.Text += "\nhello world";
-                    break;
-                case "next":
-                    richTextBox1.Text += "\nnext";
-                    break;
-                case "previous":
-                    richTextBox1.Text += "\nprevious";
-                    break;
-                case "prev":
-                    richTextBox1.Text += "\nprev";
-                    break;
+            if (e.Result.Text == "edit mode"){
+                Choices commands = new Choices();
+                commands.Add(new string[] { "hello", "hello world", "next", "previous", "prev" });
+                GrammarBuilder gBuilder = new GrammarBuilder();
+                gBuilder.Append(commands);
+                Grammar grammar = new Grammar(gBuilder);
+                recEngine2.LoadGrammarAsync(grammar);
+                recEngine2.SpeechRecognized += RecEngine_SpeechRecognized;
+                switch (e.Result.Text)
+                {
+                    case "hello":
+                        richTextBox1.Text += "\nhello";
+                        break;
+                    case "hello world":
+                        richTextBox1.Text += "\nhello world";
+                        break;
+                    case "next":
+                        richTextBox1.Text += "\nnext";
+                        break;
+                    case "previous":
+                        richTextBox1.Text += "\nprevious";
+                        break;
+                    case "prev":
+                        richTextBox1.Text += "\nprev";
+                        break;
+                    default:
+                        richTextBox1.Text += "\nedit mode";
+                        break;
+                }
             }
+            else if (e.Result.Text == "review mode")
+            {
+                Choices commands2 = new Choices();
+
+                commands2.Add(new string[] { "no", "yes" });
+
+                GrammarBuilder gBuilder2 = new GrammarBuilder();
+
+                gBuilder2.Append(commands2);
+
+                Grammar grammar2 = new Grammar(gBuilder2);
+
+                recEngine3.LoadGrammarAsync(grammar2);
+
+                recEngine3.SpeechRecognized += RecEngine_SpeechRecognized;
+
+                switch (e.Result.Text)
+                {
+                    case "no":
+                        richTextBox1.Text += "\nhello";
+                        break;
+                    case "yes":
+                        richTextBox1.Text += "\nhello world";
+                        break;
+
+                    default:
+                        richTextBox1.Text += "\nreview mode";
+                        break;
+                }
+            }
+
+
+
         }
 
         private void btnDisable_Click(object sender, EventArgs e)
